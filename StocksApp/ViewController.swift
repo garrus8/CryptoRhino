bbbd1ea5139cf1ddf6cd0ca482e3b899cb9fb069
Gregory <tolkachev.grig@gmail.com>
2021-02-25 16:03:52 +0300

    Initial Commit

---
+++ StocksApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StocksApp
+//
+//  Created by Григорий Толкачев on 25.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
